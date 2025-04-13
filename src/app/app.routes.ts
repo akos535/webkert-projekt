47960191a092f53281e7022b30942a0fca88a0b5
akos535 <csernyik.akos15@gmail.com>
2025-04-13 18:59:28 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import {HomeComponent} from "./pages/home/home.component";
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'contacts', component: ContactsComponent },
-    { path: '**', redirectTo: '' }
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,30 @@
 // home.component.ts
 import { HeaderComponent } from '../header/header.component';
 import {Component} from "@angular/core";
+import {NgStyle} from "@angular/common";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgStyle, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+  request: FormGroup;
+  selectedDevice: string = '';
+  constructor(private formBuilder: FormBuilder) {
+    this.request = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      device: [''],
+      deviceProblem: [''],
+      description: [''],
+    });
+  }
+  onDeviceChange(device: string): void {
+    this.selectedDevice = device;
+  }
+}
